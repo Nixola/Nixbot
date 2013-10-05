ce7d2d0c09501cf31d87b265e70ba35c0e777acd
Nixola <niki.or97@yahoo.it>
2013-10-05 09:41:47 +0200

    Create sandbox.lua

---
+++ sandbox.lua
@@ -0,0 +1,9 @@
+os = {time = os.time, clock = os.clock, date = os.date, difftime = os.difftime}
+io = {write = io.write}
+package.loadlib = nil
+debug = nil
+require = nil
+dofile = nil
+loadfile = nil
+loadstring = nil
+load = nil
